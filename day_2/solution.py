5f0309e7d08ca00ae9fd3ae32a460a8f353d4ac7
ANTJNSX <Antombomben@gmail.com>
2023-12-03 21:23:56 +0100

    idea set on day 2

--- day_2/solution.py
+++ day_2/solution.py
@@ -1,18 +1,14 @@
 import os
 
 
-def part_1(file):
+def part_1(file):  # extract the data into a dict
     for line in file:
-        line = str(line)
-        line = line.replace("Game ", "")
-
-        res = line.split(":")  # result with game number in the beginning
-
-        print(res)
+        pass
 
     return 0
 
 
+splitters = [":", ","]
 limit = {"red": 12, "green": 13, "blue": 14}
 path = os.getcwd() + r"/day_2/data.txt"
 
